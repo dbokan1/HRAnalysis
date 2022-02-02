f4f91987f42f39fc32e90db4a209e7dc6417e70b
Davor BOKAN <dbokan1@etf.unsa.ba>
2022-02-03 00:04:26 +0100

    Provjera duplikata train seta

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ import tensorflow as tf
 
 
 
+ds=pd.read_csv("aug_train.csv")
+# provjera duplikata u setu
+duplicate=ds["enrollee_id"].duplicated()
+for i in duplicate:
+    if i:
+        print(i)
+#0 duplikata
+
+
+
